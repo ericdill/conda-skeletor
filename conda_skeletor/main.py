c724bacf521396cfdfd4324e8029cb5158589b47
Eric Dill <edill@bnl.gov>
2015-10-16 10:32:45 -0400

    MNT: Run test requirements through a set to remove duplicates

--- conda_skeletor/main.py
+++ conda_skeletor/main.py
@@ -514,7 +514,7 @@ def execute_programmatically(skeletor_config_path, source_path, output_dir):
         run_requires.append(lib)
 
     run_requires = sorted(run_requires)
-    test_requires = sorted(test_requires + run_requires)
+    test_requires = sorted(set(test_requires + run_requires))
 
     template_info['run_requirements'] = run_requires
     template_info['test_requires'] = test_requires
