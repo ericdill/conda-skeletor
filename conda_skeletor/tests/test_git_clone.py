001d56a5c2853e3366df589fa2a9f0cdbc64f180
Eric Dill <edill@bnl.gov>
2015-10-17 16:32:16 -0400

    TST: Document the purpose of the test

--- conda_skeletor/tests/test_git_clone.py
+++ conda_skeletor/tests/test_git_clone.py
@@ -2,5 +2,7 @@ from conda_skeletor.git import clone
 
 
 def test_git_clone():
+    """Smoketest to make sure an exception isn't thrown on duplicate clone calls
+    """
     clone('http://github.com/ericdill/conda-skeletor')
     clone('http://github.com/ericdill/conda-skeletor')
